ad4001cfe8ac7ef5f6d8cd1d3781a480b701d4b2
pusparaj.dangi <pusparaj.dangi@mypay.com.np>
2024-10-15 14:51:54 +0545

    docker compose added

--- instclonebackend/keys.js
+++ instclonebackend/keys.js
@@ -1,4 +1,10 @@
+// module.exports = {
+//   MONGOURI: "mongodb+srv://pushpa:pushpa@instaclone.n2awf.mongodb.net/insta_db?retryWrites=true&w=majority",
+//   JWT_SECRET: "mysecretdon",
+// };
+
+
 module.exports = {
-  MONGOURI: "mongodb+srv://pushpa:pushpa@instaclone.n2awf.mongodb.net/insta_db?retryWrites=true&w=majority",
-  JWT_SECRET: "mysecretdon",
+  MONGOURI: process.env.MONGOURI || "mongodb://mongo:27017/instclone",
+  JWT_SECRET: process.env.JWT_SECRET || "mysecretdon",  
 };
